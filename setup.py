179214bb0bd6c511cc4f3f8a44a93f0f18228cb6
JJ Kavelaars <JJ.Kavelaars@nrc-cnrc.gc.ca>
2025-11-17 16:11:48 +0100

    Move to a EUPL and keep the build area clean

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 # setup.py
+from distutils.dir_util import remove_tree
 from glob import glob
 import os
+import pathlib
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-import pathlib
-import subprocess
 import shutil
+import subprocess
 import sys
+
+# Remove the build directory at the start
+
+if pathlib.PosixPath("build").is_dir():
+    remove_tree('build', verbose=True)
 
 
 ROOT = pathlib.Path(__file__).parent.resolve()
 FORTRAN_DIR = ROOT / "F95"
 make = shutil.which('make')
-LOG_FILE = open(f"{ROOT.joinpath("log.txt")}", "w")
-
 ext_name = "ossssimlib"
 
 
@@ -29,17 +33,12 @@ class BuildExtWithMake(build_ext):
 
     def build_extension(self, ext):
         # We assume the Makefile produced a shared library for this extension.
-        # Name it so that we can locate it deterministically.
-        # Example: for ext.name == "surveysim._simsub", Makefile builds:
-        #   fortran/_simsub.so
-        ext_name = ext.name.split(".")[-1]   # "_simsubs"
+        assert ext_name == ext.name # "ossssimlib"
         target_path = pathlib.Path(self.get_ext_fullpath(ext.name)).parent
         built_so = pathlib.Path(self.get_ext_fullpath(ext.name)).name
         built_so = FORTRAN_DIR / f"_{built_so}"
         built_py = FORTRAN_DIR / f"{ext_name}"
-        LOG_FILE.write(f"{built_so} -> {target_path}\n")
         built_so.rename(target_path/built_so.name)
-        LOG_FILE.write(f"{built_py} -> {target_path}\n")
         built_py.rename(target_path / built_py.name)
 
 
@@ -53,4 +52,6 @@ setup(
     cmdclass={"build_ext": BuildExtWithMake},
     package_dir={"": "python"},
     packages=["ossssim"],
+    license="EUPL-1.1"
 )
+
